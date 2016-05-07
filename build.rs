c8bebf1cbd6a00b095643de8856f38b95b5b1f07
Alexander Stavonin <alex@sysdev.me>
2016-05-07 22:05:38 +0800

    copying test dylib to output folder

--- build.rs
+++ build.rs
@@ -1,11 +1,14 @@
 use std::process::Command;
 use std::path::Path;
 use std::fs;
+use std::env;
 
 pub fn main() {
-    let cpplib_path = Path::new("./cpplib");
+    let cpplib_bin_name = "libcpp2rs.dylib";
+    let cpplib_str = "./cpplib";
+    let cpplib_path = Path::new(cpplib_str);
 
-    match fs::metadata(format!("{}/Makefile", cpplib_path.to_str().unwrap())) {
+    match fs::metadata(format!("{}/Makefile", cpplib_str)) {
         Err(..) => {
             Command::new("cmake").args(&["CMakeLists.txt"])
                 .current_dir(&cpplib_path).status().unwrap();
@@ -14,6 +17,14 @@ pub fn main() {
     }
     Command::new("make").current_dir(&cpplib_path).status().unwrap();
 
-    println!("cargo:rustc-link-search=native={}/bin", cpplib_path.to_str().unwrap());
+    let out_dir = format!("{}/{}",
+                          env::var("CARGO_TARGET_DIR").unwrap_or("target".to_string()),
+                          env::var("PROFILE").unwrap());
+
+    let src_file = format!("{}/bin/{}", cpplib_str, cpplib_bin_name);
+    let dst_file = format!("{}/{}", out_dir, cpplib_bin_name);
+    fs::copy(src_file, dst_file).unwrap();
+
+    println!("cargo:rustc-link-search=native={}/bin", cpplib_str);
 }
 
